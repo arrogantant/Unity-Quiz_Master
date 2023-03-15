9f4c1f99332904958a1d0615452faf75b359681d
Junseong <david991129@naver.com>
2023-03-16 00:33:57 +0900

    Add Question text

--- Assets/Cs/QuestionSO.cs
+++ Assets/Cs/QuestionSO.cs
@@ -6,5 +6,19 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
-    [SerializeField] string Question = "Enter new quesrion text here";
+    [SerializeField] string question = "Enter new quesrion text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex;
 }
